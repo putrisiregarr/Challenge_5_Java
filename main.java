cd370785a042f94c5e8f82a69709a081ba769b95
putrisiregarr <siregarrputri20@gmail.com>
2021-10-22 21:17:05 +0700

    first commit

---
+++ main.java
@@ -0,0 +1,42 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Dwi Putri", 21, 160);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      
+      hero.lari();
+
+      hero.makan();
+      hero.makan();
+
+      hero.minum();
+      hero.minum();
+      hero.minum();
+
+      hero.lompat();
+
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      
+      
+      // tampilkan status terkini...
+      hero.profile();
+      
+    }
+  
+    
+}
